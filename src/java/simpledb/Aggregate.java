d2281387fed7f80a144c6cf97315f3ba2cb31709
Abner Zheng <abnerzheng@gmail.com>
2018-10-15 20:38:47 +0800

    finish aggregate

--- src/java/simpledb/Aggregate.java
+++ src/java/simpledb/Aggregate.java
@@ -10,8 +10,14 @@ import java.util.*;
 public class Aggregate extends Operator {
 
     private static final long serialVersionUID = 1L;
+	private final Aggregator aggregator;
+	private OpIterator child;
+	private final int afield;
+	private final int gfield;
+	private final Aggregator.Op aop;
+	private OpIterator aggIter;
 
-    /**
+	/**
      * Constructor.
      * 
      * Implementation hint: depending on the type of afield, you will want to
@@ -30,7 +36,17 @@ public class Aggregate extends Operator {
      *            The aggregation operator to use
      */
     public Aggregate(OpIterator child, int afield, int gfield, Aggregator.Op aop) {
-	// some code goes here
+    	this.child = child;
+    	this.afield = afield;
+    	this.gfield = gfield;
+    	this.aop = aop;
+	    final Type aType = child.getTupleDesc().getFieldType(afield);
+	    final Type gType = child.getTupleDesc().getFieldType(gfield);
+	    if(aType == Type.INT_TYPE){
+	    	this.aggregator = new IntegerAggregator(gfield,gType,afield, aop);
+	    }else{
+	    	this.aggregator = new StringAggregator(gfield, gType,afield, aop);
+	    }
     }
 
     /**
@@ -39,8 +55,7 @@ public class Aggregate extends Operator {
      *         {@link simpledb.Aggregator#NO_GROUPING}
      * */
     public int groupField() {
-	// some code goes here
-	return -1;
+    	return gfield;
     }
 
     /**
@@ -49,16 +64,14 @@ public class Aggregate extends Operator {
      *         null;
      * */
     public String groupFieldName() {
-	// some code goes here
-	return null;
+    	return child.getTupleDesc().getFieldName(gfield);
     }
 
     /**
      * @return the aggregate field
      * */
     public int aggregateField() {
-	// some code goes here
-	return -1;
+    	return afield;
     }
 
     /**
@@ -66,72 +79,80 @@ public class Aggregate extends Operator {
      *         tuples
      * */
     public String aggregateFieldName() {
-	// some code goes here
-	return null;
+    	return this.child.getTupleDesc().getFieldName(afield);
     }
 
     /**
      * @return return the aggregate operator
      * */
     public Aggregator.Op aggregateOp() {
-	// some code goes here
-	return null;
+    	return this.aop;
     }
 
-    public static String nameOfAggregatorOp(Aggregator.Op aop) {
-	return aop.toString();
-    }
+	public static String nameOfAggregatorOp(Aggregator.Op aop) {
+		return aop.toString();
+	}
 
-    public void open() throws NoSuchElementException, DbException,
-	    TransactionAbortedException {
-	// some code goes here
-    }
+	public void open() throws NoSuchElementException, DbException,
+		                          TransactionAbortedException {
+    	super.open();
+    	child.open();
+		while(child.hasNext()){
+			this.aggregator.mergeTupleIntoGroup(child.next());
+		}
+		child.close();
+		this.aggIter = this.aggregator.iterator();
+		this.aggIter.open();
+	}
 
-    /**
-     * Returns the next tuple. If there is a group by field, then the first
-     * field is the field by which we are grouping, and the second field is the
-     * result of computing the aggregate. If there is no group by field, then
-     * the result tuple should contain one field representing the result of the
-     * aggregate. Should return null if there are no more tuples.
-     */
-    protected Tuple fetchNext() throws TransactionAbortedException, DbException {
-	// some code goes here
-	return null;
-    }
+	/**
+	 * Returns the next tuple. If there is a group by field, then the first
+	 * field is the field by which we are grouping, and the second field is the
+	 * result of computing the aggregate. If there is no group by field, then
+	 * the result tuple should contain one field representing the result of the
+	 * aggregate. Should return null if there are no more tuples.
+	 */
+	protected Tuple fetchNext() throws TransactionAbortedException, DbException {
+		if(this.aggIter.hasNext()) {
+			return this.aggIter.next();
+		}else{
+			return null;
+		}
+	}
 
-    public void rewind() throws DbException, TransactionAbortedException {
-	// some code goes here
-    }
+	public void rewind() throws DbException, TransactionAbortedException {
+		this.aggIter.rewind();
+	}
 
-    /**
-     * Returns the TupleDesc of this Aggregate. If there is no group by field,
-     * this will have one field - the aggregate column. If there is a group by
-     * field, the first field will be the group by field, and the second will be
-     * the aggregate value column.
-     * 
-     * The name of an aggregate column should be informative. For example:
-     * "aggName(aop) (child_td.getFieldName(afield))" where aop and afield are
-     * given in the constructor, and child_td is the TupleDesc of the child
-     * iterator.
-     */
-    public TupleDesc getTupleDesc() {
-	// some code goes here
-	return null;
-    }
+	/**
+	 * Returns the TupleDesc of this Aggregate. If there is no group by field,
+	 * this will have one field - the aggregate column. If there is a group by
+	 * field, the first field will be the group by field, and the second will be
+	 * the aggregate value column.
+	 *
+	 * The name of an aggregate column should be informative. For example:
+	 * "aggName(aop) (child_td.getFieldName(afield))" where aop and afield are
+	 * given in the constructor, and child_td is the TupleDesc of the child
+	 * iterator.
+	 */
+	public TupleDesc getTupleDesc() {
+		return this.aggregator.iterator().getTupleDesc();
+	}
 
-    public void close() {
-	// some code goes here
-    }
+	public void close() {
+		super.close();
+		this.aggIter.close();
+	}
 
-    @Override
-    public OpIterator[] getChildren() {
-	// some code goes here
-	return null;
-    }
+	@Override
+	public OpIterator[] getChildren() {
+		return new OpIterator[]{child};
+	}
 
-    @Override
-    public void setChildren(OpIterator[] children) {
-	// some code goes here
-    }
-    
+	@Override
+	public void setChildren(OpIterator[] children) {
+		assert children.length == 1;
+		child = children[0];
+	}
+
 }
